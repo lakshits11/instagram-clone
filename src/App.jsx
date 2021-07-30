5528d6181f8cc4f25c81f0cb71b1e39381ee9fbb
lakshits11 <lakshits11@gmail.com>
2021-07-30 17:29:09 +0530

    Modified Some necessary files - first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <h1>Hello Lakshit The Great!!🚀</h1>
+    </div>
+  );
+}
+
+export default App;
